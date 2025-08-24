802e814226d89c313573ed96f0d87eaef8ddd6f0
Jose Luis <jlestacio90@gmail.com>
2025-08-23 20:50:28 -0500

    iniciando repositorio

---
+++ app.js
@@ -0,0 +1,30 @@
+const inputAmigo = document.getElementById("amigo");
+const listaAmigos = [];
+const ulListaAmigos = document.getElementById ("listaAmigos");
+const ulResultado = document.getElementById("resultado");
+function agregarAmigo( ){
+    if (!inputAmigo.value); {
+    alert("Debes ingresar un nombre");
+    }
+    listaAmigos.push(inputAmigo.value); 
+    //ulListaAmigos.innerHTML= ulListaAmigos.innerHTML + inputAmigo.value;
+    ulListaAmigos.innerHTML += `<li>${inputAmigo.value}</li>`;
+};
+
+function sortearAmigo(){
+    const random = Math.floor((Math.random () * listaAmigos.length));
+    const amigoSecreto = listaAmigos[random];
+    ulResultado.innerHTML = `<li>El amigo secreto es: ${amigoSecreto}</li>`;
+}
+
+
+
+
+
+
+// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+//Variables
+//Arreglos
+//Funciones
+//Eventos
+//objetos   
